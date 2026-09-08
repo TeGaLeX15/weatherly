42cbfdbf874fd8612a7091124707eac865d89c8d
tegalex15 <gartartem1505@gmail.com>
2026-09-08 14:47:24 +0500

    feat: build initial weather dashboard

---
+++ Views/Components/WeatherScene.axaml.cs
@@ -0,0 +1,12 @@
+// Views/Components/WeatherScene.axaml.cs
+using Avalonia.Controls;
+
+namespace Weatherly.Views.Components;
+
+public partial class WeatherScene : UserControl
+{
+    public WeatherScene()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+// Views/MainView.axaml.cs
+using Avalonia.Controls;
+
+namespace Weatherly.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
